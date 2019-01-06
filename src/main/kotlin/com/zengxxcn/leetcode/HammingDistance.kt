616963a2503ee51fe5cf031cba4a75fc873c7304
zengxxcn <zengxxcn@gmail.com>
2019-01-06 12:06:46 +1100

    Add hamming distance

--- src/main/kotlin/com/zengxxcn/leetcode/HammingDistance.kt
+++ src/main/kotlin/com/zengxxcn/leetcode/HammingDistance.kt
@@ -1,2 +1,25 @@
 package com.zengxxcn.leetcode
 
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * https://leetcode.com/problems/hamming-distance/
+ */
+class HammingDistance {
+
+    fun hammingDistance(x: Int, y: Int): Int {
+        var distance = 0
+        for (i in 0..31) {
+            distance += ((x shr i) xor (y shr i)).rem(2)
+        }
+        return distance
+    }
+
+    @Test
+    fun test() {
+        assertEquals(2, hammingDistance(1, 4))
+        assertEquals(1, hammingDistance(3, 1))
+        assertEquals(31, hammingDistance(0, Integer.MAX_VALUE))
+    }
+}
