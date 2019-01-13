ebcac4240f6e5f8ee25a53df79c1b19484c01bdd
zengxxcn <zengxxcn@gmail.com>
2019-01-13 18:46:12 +1100

    Climb stairs

---
+++ src/main/kotlin/com/zengxxcn/leetcode/ClimbStairs.kt
@@ -0,0 +1,27 @@
+package com.zengxxcn.leetcode
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+class ClimbStairs {
+    fun climbStairs(n: Int): Int {
+        return when (n) {
+            1 -> 1
+            2 -> 2
+            else -> {
+                climbStairs(n - 2) + climbStairs(n - 1)
+            }
+        }
+
+    }
+
+    @Test
+    fun test() {
+        assertEquals(3, climbStairs(3))
+        assertEquals(5, climbStairs(4))
+        assertEquals(8, climbStairs(5))
+    }
+}
