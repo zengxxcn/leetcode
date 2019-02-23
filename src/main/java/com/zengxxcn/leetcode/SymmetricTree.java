3d6ad9ef43f2677fe0eec7c9c97a9f3ea442e05c
zengxxcn <zengxxcn@gmail.com>
2019-02-23 15:58:53 +1100

    symmetric tree

---
+++ src/main/java/com/zengxxcn/leetcode/SymmetricTree.java
@@ -0,0 +1,58 @@
+package com.zengxxcn.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Problem 101: https://leetcode.com/problems/symmetric-tree
+ */
+public class SymmetricTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return compare(root.left, root.right);
+    }
+
+    private boolean compare(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        else if (left == null && right != null) return false;
+        else if (right == null && left != null) return false;
+        else if (left.val != right.val) return false;
+        else return compare(left.left, right.right) && compare(left.right, right.left);
+    }
+
+    @Test
+    public void test1() {
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(2);
+        treeNode.left.left = new TreeNode(3);
+        treeNode.left.right = new TreeNode(4);
+        treeNode.right.left = new TreeNode(4);
+        treeNode.right.right = new TreeNode(3);
+
+        assertTrue(isSymmetric(treeNode));
+    }
+
+    @Test
+    public void test2() {
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(2);
+        treeNode.left.right = new TreeNode(4);
+        treeNode.right.right = new TreeNode(4);
+
+        assertFalse(isSymmetric(treeNode));
+    }
+}
