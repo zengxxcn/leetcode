eb0486e8afbfd3ebbac0ac2df35c8849ceba8a29
Xiaoxuan Zeng <xiaoxuan.zeng@anz.com>
2019-09-08 12:30:12 +1000

    Binary search

---
+++ src/main/kotlin/com/zengxxcn/leetcode/BinarySearch.kt
@@ -0,0 +1,32 @@
+package com.zengxxcn.leetcode
+
+import org.junit.Assert
+import org.junit.Test
+
+class BinarySearch {
+
+    fun search(nums: IntArray, target: Int): Int {
+        return binarySearch(nums, target, 0, nums.size - 1)
+    }
+
+    fun binarySearch(nums: IntArray, target: Int, startIndex: Int, endIndex: Int): Int {
+        if (startIndex == endIndex)
+            return if (nums[startIndex] == target) startIndex else -1
+
+        val midIndex = startIndex + (endIndex - startIndex) / 2
+
+        val midValue = nums[midIndex]
+        return when {
+            midValue == target -> midIndex
+            midValue > target -> if (midIndex == startIndex) -1 else binarySearch(nums, target, startIndex, midIndex - 1)
+            else -> if (midIndex == endIndex) -1 else binarySearch(nums, target, midIndex + 1, endIndex)
+        }
+    }
+
+    @Test
+    fun test1() {
+        Assert.assertEquals(search(intArrayOf(-1, 0, 3, 5, 9, 12), 9), 4)
+        Assert.assertEquals(search(intArrayOf(-1, 0, 3, 5, 9, 12), 2), -1)
+        Assert.assertEquals(search(intArrayOf(2, 5), 0), -1)
+    }
+}
