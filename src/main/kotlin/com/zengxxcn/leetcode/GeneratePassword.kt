594073e5040de85143fc78c9f61f7e7ee6aea554
Jim Zeng <xiaoxuan.zeng@anz.com>
2020-08-08 12:28:16 +1000

    improve GeneratePassword

--- src/main/kotlin/com/zengxxcn/leetcode/GeneratePassword.kt
+++ src/main/kotlin/com/zengxxcn/leetcode/GeneratePassword.kt
@@ -1,8 +1,9 @@
 package com.zengxxcn.leetcode
 
-import java.lang.StringBuilder
+import org.junit.Assert
+import org.junit.Test
 
-object PasswordGenerator {
+class PasswordGenerator {
 
     /**
      * Generate passwords by replacing some characters with provided replacements in all combinations.
@@ -10,85 +11,75 @@ object PasswordGenerator {
      * seed = "apple", replacements = { "a": ["@", "4"], "p": ["%"], "z": ["^"] }
      * returns = ["apple", "@pple", "4pple", "a%ple", "ap%le", "@%ple", "4%ple", "@p%le", "4p%le", "a%%le", "@%%le", "4%%le"]
      */
-    fun generatePasswords(seed: String, replacements: Map<Char, List<String>>): List<String> {
-        val indexMap = mutableMapOf<Int, Char>()
-        seed.forEachIndexed { index, seedChar ->
-            replacements.keys.forEach { replacement ->
-                if (seedChar == replacement) {
-                    indexMap[index] = seedChar
-                }
-            }
-        }
-
-        //exhaust all combinations of indexes
-        val indexesList = generateSubLists(indexMap.keys.toList(), 0)
-        println("Combinations: $indexesList")
-
-        //generate password as per each combination
-        val passwords = mutableListOf(seed)
-        for (combination in indexesList) {
-            val posReplacements = combination.map {
-                val char = seed[it]
-                replacements.getValue(char)
-            }
-
-            val replacementList = combine(posReplacements, 0)
-            replacementList.forEach { replacementCombination ->
-                val buf = StringBuilder(seed)
-                combination.forEachIndexed { index, position ->
-                    buf.replace(position, position+1, replacementCombination[index])
-                }
-                passwords.add(buf.toString())
-            }
-        }
-        return passwords
+    fun genPasswd(seed: String, replacements: Map<Char, List<String>>): List<String> {
+        val strList = expandSeedWithReplacements(seed, replacements)
+        return combine(strList, 0)
     }
 
-
-    private fun generateSubLists(input: List<Int>, index: Int): List<List<Int>> {
-        if (index < input.size) {
-            val value = input[index]
-            val list = mutableListOf(listOf(value)) //its own
-            val subCombinations = generateSubLists(input, index+1)
-            list.addAll(subCombinations)
-            subCombinations.forEach {
-                val newList = mutableListOf(value)
-                newList.addAll(it)
-                list.add(newList)
+    /**
+     * Expand every seed character to a list of candidate strings.
+     * e.g.
+     * 'a' -> "a", "@", "4"
+     * 'p' -> "p", "%"
+     * 'p' -> "p", "%"
+     * 'l' -> "l"
+     * 'e' -> "e"
+     */
+    private fun expandSeedWithReplacements(seed: String, replacements: Map<Char, List<String>>): List<List<String>> {
+        return seed.map {
+            val strList = mutableListOf(it.toString())
+            replacements[it]?.forEach { replacement ->
+                strList.add(replacement)
             }
-            return list
+            strList.toList()
+        }
+    }
+
+    /**
+     * Generate all combinations of possible strings.
+     *
+     * It goes depth-first traverse recursively.
+     *
+     * @param input list of List<String>
+     * e.g.
+     * [0]: "a", "@", "4"
+     * [1]: "p", "%"
+     * [2]: "p", "%"
+     * [3]: "l"
+     * [4]: "e"
+     *
+     * @param index start from
+     *
+     * @return all combination of strings from index
+     * e.g.
+     * index = 4: ["e"]
+     * index = 3: ["le"]
+     * index = 2: ["ple", "%le"]
+     * index = 1: ["pple", "p%le", "%ple", "%%le"]
+     * index = 0: ["apple", "ap%le", "a%ple", "a%%le", "@pple", "@p%le", "@%ple", "@%%le", "4pple", "4p%le", "4%ple", "4%%le"]
+     */
+    private fun combine(input: List<List<String>>, index: Int): List<String> {
+        if (index < input.size - 1) {
+            val currCandidates = input[index]
+            val subCombined = combine(input, index + 1)
+            val ret = mutableListOf<String>()
+            currCandidates.forEach { value ->
+                subCombined.forEach { subValues ->
+                    ret.add(value + subValues)
+                }
+            }
+            return ret
         } else {
-            return emptyList()
+            // index == input.size - 1
+            return input[index]
         }
     }
 
-    private fun combine(input: List<List<String>>, index: Int): List<List<String>> {
-        if (index < input.size) {
-            val values = input[index]
-            val subCombined = combine(input, index+1)
-            if (subCombined.isNotEmpty()) {
-                val ret = mutableListOf<List<String>>()
-                values.forEach { value ->
-                    subCombined.forEach { subValues ->
-                        val list = mutableListOf(value)
-                        list.addAll(subValues)
-                        ret.add(list)
-                    }
-                }
-                return ret
-            } else {
-                return listOf(values)
-            }
-        } else {
-            return emptyList()
-        }
-
+    @Test
+    fun test() {
+        val pwds = genPasswd("apple", mapOf( 'a' to listOf("@", "4"), 'p' to listOf("%"), 'z' to listOf("^") ))
+        println("Generated ${pwds.size} passwords: $pwds")
+        Assert.assertEquals(3 * 2 * 2 * 1 * 1, pwds.size)
     }
 
-}
-
-fun main(args: Array<String>) {
-    val pwds = PasswordGenerator.generatePasswords("apple",
-            mapOf( 'a' to listOf("@", "4"), 'p' to listOf("%"), 'z' to listOf("^") ))
-    println("Generated ${pwds.size} passwords: $pwds")
 }
